4cf8d90f8db93704dfef682c4ea35a0d2ca020d5
DmitryDoskolovich <dmitriy.doskolovich@gmail.com>
2019-11-21 13:20:55 +0400

    add params updateUser

--- app/usersModule.js
+++ app/usersModule.js
@@ -11,12 +11,22 @@ async function isAdmin(userId) {
   return user.email === process.env.ADMIN_EMAIL;
 }
 
-async function updateUser(userId, payload, transaction) {
-  const user = await db.User.findById(userId, { transaction });
+async function updateUser(userId, payload, params) {
+  const user = await db.User.findById(userId, {
+    transaction: params
+      ? params.transaction
+        ? params.transaction
+        : undefined
+      : undefined
+  });
   if (user) {
     await user.update(payload, {
       fields: ["name", "photoUrl", "rating"],
-      transaction
+      transaction: params
+        ? params.transaction
+          ? params.transaction
+          : undefined
+        : undefined
     });
   }
   return user;
