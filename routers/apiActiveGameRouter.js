6c189bf73bfa48bfaa0554d1ae85d0c737866910
Alexey Kuznetsov <a.kuznetsov@mercurydevelopment.com>
2021-08-27 11:55:32 +0400

    Broadcast game players state

--- routers/apiActiveGameRouter.js
+++ routers/apiActiveGameRouter.js
@@ -2,10 +2,58 @@ const Router = require("koa-router");
 const sse = require("koa-sse-stream");
 const EventEmitter = require("events");
 
-class ActiveGameEmitter extends EventEmitter {}
-const activeGameEmitter = new ActiveGameEmitter();
+const gameEventEmitter = new EventEmitter();
 
 const apiActiveGameRouter = new Router();
+
+const TeamPosition = {
+  Red: 0,
+  Blue: 1
+};
+
+const PlayerPosition = {
+  Forward: 0,
+  Defender: 1
+};
+
+const gameSlots = [
+  {
+    team: TeamPosition.Red,
+    position: PlayerPosition.Defender,
+    user: null
+  },
+  {
+    team: TeamPosition.Blue,
+    position: PlayerPosition.Forward,
+    user: null
+  },
+  {
+    team: TeamPosition.Red,
+    position: PlayerPosition.Forward,
+    user: null
+  },
+  {
+    team: TeamPosition.Blue,
+    position: PlayerPosition.Defender,
+    user: null
+  }
+];
+
+function handleSelectPlayer({ team, position, user }) {
+  const gameSlot = gameSlots.find(
+    gameSlot => gameSlot.position === position && gameSlot.team === team
+  );
+  const prevGameSlot = gameSlots.find(
+    gameSlot => gameSlot.user && gameSlot.user.id === user.id
+  );
+  if (prevGameSlot) {
+    prevGameSlot.user = gameSlot.user;
+  }
+  gameSlot.user = user;
+}
+
+const ACTION_SELECT_PLAYER = "select-player";
+const ACTION_UPDATE_ALL_PLAYERS = "update-all-players";
 
 apiActiveGameRouter
   .get(
@@ -15,19 +63,31 @@ apiActiveGameRouter
       pingInterval: 30000
     }),
     async ctx => {
-      function sendEventToClient(data) {
-        ctx.sse.send(JSON.stringify(data));
+      const dispatch = payload => ctx.sse.send(JSON.stringify(payload));
+
+      function handleEvent({ type, payload }) {
+        switch (type) {
+          case ACTION_SELECT_PLAYER:
+            handleSelectPlayer(payload);
+            dispatch({
+              type: ACTION_UPDATE_ALL_PLAYERS,
+              payload: { gameSlots }
+            });
+            break;
+        }
       }
 
-      activeGameEmitter.on("event", sendEventToClient);
+      dispatch({ type: ACTION_UPDATE_ALL_PLAYERS, payload: { gameSlots } });
+
+      gameEventEmitter.on("event", handleEvent);
       ctx.sse.on("close", () => {
-        activeGameEmitter.removeListener("event", sendEventToClient);
+        gameEventEmitter.removeListener("event", handleEvent);
       });
     }
   )
   .post("/api/active-game/events", async ctx => {
-    activeGameEmitter.emit("event", ctx.request.body);
-    ctx.body = ctx.request.body;
+    gameEventEmitter.emit("event", ctx.request.body);
+    ctx.status = 204;
   });
 
 module.exports = apiActiveGameRouter;
